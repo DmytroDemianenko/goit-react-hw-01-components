d461d1b280fc6034267f050bd0ad6538feb52fa2
Dmytro Demianenko <83134601+DmytroDemianenko@users.noreply.github.com>
2021-12-18 21:49:39 +0100

    16/12

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
- import PaintingList from './components/PaintingList';
- import Section from './components/Section';
- import paintings from './painting.json';
+//  import PaintingList from './components/PaintingList';
+import Profile from './components/Profile/Profile';
+import Section from './components/Section';
+import user from './components/Profile/user.json';
+//  import paintings from './painting.json';
  export default function App() {
      return (<div>
          <Section title="Top week">
-         <PaintingList items={paintings} />
+             <Profile userName={user.username} tag={user.tag} location={user.location} avatar={user.avatar} followers={user.stats.followers} views={user.stats.views} likes={user.stats.likes}/>
          </Section>
      </div>);
  }
